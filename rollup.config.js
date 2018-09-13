903514db43b72a9f3d4ee18916425c6a26923278
Andersh75 <andersh75@gmail.com>
2018-09-13 17:33:23 +0200

    from nm

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+rollup.config.js
+export default [{
+  entry: "src/index.js",
+  dest: "dist/index.js",
+  format: "cjs"
+}, {
+  entry: "src/index.js",
+  dest: "dist/index.es6.js",
+  format: 'es'
+}]
